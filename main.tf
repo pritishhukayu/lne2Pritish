06e0021c04d5102c21714c31c9544206860a8aec
PritishK <91003769+pritishhukayu@users.noreply.github.com>
2024-06-22 12:53:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 provider "azurerm" {
   features {}
 }
+
+# Define your resource group
+resource "azurerm_resource_group" "my_rg" {
+  name     = "lne-PritishRG"
+  location = "Central India"
+}
+
+# Define your Azure Container Registry
 resource "azurerm_container_registry" "my_acr" {
-  name                     = "lne-PritishRG"
-  resource_group_name      = azurerm_resource_group.my_rg.name
-  location                 = azurerm_resource_group.my_rg.location
+  name                     = "lne2ACR"
+  resource_group_name      = azurerm_resource_group.my_rg.name  # Reference the resource group name directly
+  location                 = azurerm_resource_group.my_rg.location  # Reference the resource group location directly
   sku                      = "Basic"
   admin_enabled            = true
 }
